f2b44b50a31597a92f10836cc14ace9d3edfba34
StilianSaka <stilisaka@gmail.com>
2024-07-25 11:16:23 +0000

    adding function case

--- print_functions.c
+++ print_functions.c
@@ -1,88 +1,95 @@
-	#include "main.h"
-	#include <stdarg.h>
-	#include <stdlib.h>
+#include "main.h"
+#include <stdarg.h>
+#include <stdlib.h>
+#include <limits.h>
 
-	/**
-	* pr_char - Prints a single character
-	* @char_list: va_list containing the character to print
-	*
-	* Return: Number of characters printed
-	*/
-	int pr_char(va_list char_list)
-	{
-		char letter = va_arg(char_list, int);
+/**
+ * pr_char - Prints a single character
+ * @char_list: va_list containing the character to print
+ *
+ * Return: Number of characters printed
+ */
+int pr_char(va_list char_list)
+{
+    char letter = va_arg(char_list, int);
+    _putchar(letter);
+    return (1);
+}
 
-		_putchar(letter);
+/**
+ * pr_string - Prints a string
+ * @string_list: va_list containing the string to print
+ *
+ * Return: Number of characters printed
+ */
+int pr_string(va_list string_list)
+{
+    int i;
+    char *word = va_arg(string_list, char*);
 
-		return (1);
-	}
+    if (word == NULL)
+        word = "(null)";
 
-	/**
-	* pr_string - Prints a string
-	* @string_list: va_list containing the string to print
-	*
-	* Return: Number of characters printed
-	*/
-	int pr_string(va_list string_list)
-	{
-		int i;
-		char *word = va_arg(string_list, char*);
+    for (i = 0; word[i] != '\0'; i++)
+        _putchar(word[i]);
 
-		if (word == NULL)
-			word = "(null)";
+    return (i);
+}
 
-		for (i = 0; word[i] != '\0'; i++)
-			_putchar(word[i]);
-
-		return (i);
-	}
-
-	/**
-	* pr_int - Prints an integer
-	* @int_list: va_list containing the integer to print
-	*
-	* Return: Number of characters printed
-	*/
-	int pr_int(va_list int_list)
-	{
-		int num = va_arg(int_list, int);
-		int len = 0;
-		char buffer[12];
-		int i = 0, is_negative = 0;
-
-
-		if (num == 0)
-		{
-			_putchar('0');
-			return (1);
-		}
+/**
+ * pr_int - Prints an integer
+ * @int_list: va_list containing the integer to print
+ *
+ * Return: Number of characters printed
+ */
+int pr_int(va_list int_list)
+{
+    int num = va_arg(int_list, int);
+    int len = 0;
+    char buffer[12];
+    int i = 0, is_negative = 0;
 
 
-		if (num < 0)
-		{
-			is_negative = 1;
-			num = -num;
-		}
+    if (num < 0) {
+        is_negative = 1;
+
+        if (num == INT_MIN) {
+            num = INT_MAX;
+        } else {
+            num = -num;
+        }
+    }
 
 
-		while (num > 0)
-		{
-			buffer[i++] = (num % 10) + '0';
-			num /= 10;
-		}
+    while (num > 0) {
+        buffer[i++] = (num % 10) + '0';
+        num /= 10;
+    }
 
 
-		if (is_negative)
-		{
-			buffer[i++] = '-';
-		}
+    if (is_negative) {
+
+        if (num == INT_MAX) {
+            buffer[0] = '8';
+            buffer[i++] = '4';
+            buffer[i++] = '6';
+            buffer[i++] = '3';
+            buffer[i++] = '7';
+            buffer[i++] = '4';
+            buffer[i++] = '8';
+            buffer[i++] = '1';
+            buffer[i++] = '2';
+            buffer[i++] = '-';
+        } else {
+            buffer[i++] = '-';
+        }
+    }
 
 
-		for (i--; i >= 0; i--)
-		{
-			_putchar(buffer[i]);
-			len++;
-		}
+    for (i--; i >= 0; i--) {
+        _putchar(buffer[i]);
+        len++;
+    }
 
-		return (len);
-	}
+    return len;
+}
