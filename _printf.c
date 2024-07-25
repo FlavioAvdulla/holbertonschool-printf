d8da091c132eebf28c36914468bd9aff6ac39792
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-25 13:02:03 +0000

    comments

--- _printf.c
+++ _printf.c
@@ -2,7 +2,6 @@
 	#include <stdarg.h>
 	#include <stdlib.h>
 
-    // Forward declarations of helper functions
 	int handle_format(const char *format, va_list arg_list, int *i);
 	int print_char(char c);
 
@@ -15,37 +14,33 @@
 	*/
 	int _printf(const char *format, ...)
 	{
-		va_list arg_list; // List to hold the variable arguments
-		int i = 0, len = 0; // 'i' is the index for format string, 'len' is the length of printed characters
+		va_list arg_list;
+		int i = 0, len = 0;
 
-		va_start(arg_list, format); // Initialize the argument list
+		va_start(arg_list, format);
 
 		if (format == NULL)
-			return (-1); // Return -1 if format string is NULL
+			return (-1);
 
-        // Loop through each character in the format string
 		while (format[i] != '\0')
 		{
 			if (format[i] == '%' && format[i + 1] != '\0')
 			{
-                // Handle format specifier
 				len += handle_format(format, arg_list, &i);
 			}
 			else if (format[i] == '%' && format[i + 1] == '\0')
 			{
-                // If '%' is the last character, return -1
 				return (-1);
 			}
 			else
 			{
-                // Print regular character
 				len += print_char(format[i]);
 			}
-			i++; // Move to the next character
+			i++;
 		}
 
-		va_end(arg_list); // Clean up the argument list
-		return (len); // Return the total length of printed characters
+		va_end(arg_list);
+		return (len);
 	}
 
 	/**
@@ -59,7 +54,7 @@
 	int handle_format(const char *format, va_list arg_list, int *i)
 	{
 		int j, len = 0;
-        // Array of structures to map format specifiers to their corresponding functions
+
 		print_data p_func[] = {
 			{"c", pr_char},
 			{"s", pr_string},
@@ -67,32 +62,30 @@
 			{"i", pr_int},
 			{NULL, NULL}
 		};
-        // Loop through the mapping array to find the matching format specifier
+
 		for (j = 0; p_func[j].type != NULL; j++)
 		{
 			if (format[*i + 1] == p_func[j].type[0])
 			{
-                // Call the appropriate function and update the length
 				len += p_func[j].print(arg_list);
 				(*i)++;
 				return (len);
 			}
 		}
-        // Handle "%%" which should print a single "%"
+
 		if (format[*i + 1] == '%')
 		{
 			len += print_char('%');
-			(*i)++; // Move index to skip the format specifier
+			(*i)++;
 		}
 		else
 		{
-            // Handle unrecognized format specifier by printing it literally
 			len += print_char('%');
 			len += print_char(format[*i + 1]);
 			(*i)++;
 		}
 
-		return (len); // Return the length of printed characters for the format specifier
+		return (len);
 	}
 
 	/**
@@ -103,6 +96,6 @@
 	*/
 	int print_char(char c)
 	{
-		_putchar(c); // Use _putchar function to print the character
-		return (1); // Return 1 since one character is printed
+		_putchar(c);
+		return (1);
 	}
