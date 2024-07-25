d48cfa5d31728324c40a8085aad262b2a3aaf8ec
Flavio Avdulla <a.flavio4366@gmail.com>
2024-07-25 06:56:50 +0000

    changes

--- _printf.c
+++ _printf.c
@@ -11,10 +11,10 @@
 */
 int _printf(const char *format, ...)
 {
-	va_list arg_list;
-	int j, i = 0, len = 0;
+	va_list arg_list; // Declare a variable to hold the list of arguments
+	int j, i = 0, len = 0; // Declare and initialize counters
 
-
+	// Array of structures mapping format specifiers to their corresponding functions
 	print_data p_func[] = {
 		{"c", pr_char},
 		{"s", pr_string},
@@ -23,39 +23,42 @@ int _printf(const char *format, ...)
 		{NULL, NULL}
 	};
 
-	va_start(arg_list, format);
+	va_start(arg_list, format); // Initialize the argument list
 
-
+	// Check if the format string is NULL
 	if (format == NULL)
 		return (0);
-
+	// Iterate through each character in the format string
 	while (format[i] != '\0') {
 		if (format[i] == '%' && format[i+1] != '%') 
 		{
 			j = 0;
-
+			// Search for the corresponding function for the format specifier
 			while (p_func[j].type != NULL)
 			{
 				if (format[i+1] == p_func[j].type[0])
 				{
+					// Call the corresponding function and update the length
 					len += p_func[j].print(arg_list);
 					i++;
 					break;
 				}
 				j++;
 			}
-
+			// If no matching function is found, print the '%' character
 			if (p_func[j].type == NULL) {
 				_putchar('%');
 				len++;
 			}
 		}
+		// Handle the case where '%%' is encountered, print a single '%'
 		else if (format[i] == '%' && format[i+1] == '%')
 		{
 			_putchar('%');
 			len++;
 			i++;
 		}
+		// Handle regular characters, print them as is
 		else 
 		{
 			_putchar(format[i]);
@@ -64,6 +67,6 @@ int _printf(const char *format, ...)
 		i++;
 	}
 
-	va_end(arg_list);
-	return (len);    
+	va_end(arg_list); // Clean up the argument list
+	return (len); // Return the total length of characters printed
 }
