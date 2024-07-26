64ab160668d45ba8e6c7a5713a8c2db0fb8e27f2
StilianSaka <stilisaka@gmail.com>
2024-07-26 06:35:30 +0000

    modification

--- _printf.c
+++ _printf.c
@@ -6,104 +6,96 @@ int handle_format(const char *format, va_list arg_list, int *i);
 int print_char(char c);
 
 /**
-* _printf - Custom printf function
-* @format: Format string containing the directives
-* @...: Additional arguments based on format specifiers
-*
-* Description: This function behaves similarly to the standard printf function,
-* processing the formatstring and printing the corresponding arguments based on
-* the format specifiers. It supports '%c', '%s', '%d', '%i', and '%%'.
-*
-* Return: Number of characters printed or -1 if format is NULL
-*/
+ * _printf - Custom printf function
+ * @format: Format string containing the directives
+ * @...: Additional arguments based on format specifiers
+ *
+ * Return: Number of characters printed or -1 if format is NULL
+ */
 int _printf(const char *format, ...)
 {
-	va_list arg_list;
-	int i = 0, len = 0;
+    va_list arg_list;
+    int i = 0, len = 0;
 
-	va_start(arg_list, format);
+    va_start(arg_list, format);
 
-	if (format == NULL)
-		return (-1);
+    if (format == NULL)
+        return (-1);
 
-	while (format[i] != '\0')
-	{
-		if (format[i] == '%' && format[i + 1] != '\0')
-		{
-			len += handle_format(format, arg_list, &i);
-		}
-		else if (format[i] == '%' && format[i + 1] == '\0')
-		{
-			return (-1);
-		}
-		else
-		{
-			len += print_char(format[i]);
-		}
-		i++;
-	}
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%' && format[i + 1] != '\0')
+        {
+            len += handle_format(format, arg_list, &i);
+        }
+        else if (format[i] == '%' && format[i + 1] == '\0')
+        {
+            return (-1);
+        }
+        else
+        {
+            len += print_char(format[i]);
+        }
+        i++;
+    }
 
-	va_end(arg_list);
-	return (len);
+    va_end(arg_list);
+    return (len);
 }
 
 /**
-* handle_format - Handles format specifiers and prints the output
-* @format: Format string containing the directives
-* @arg_list: List of arguments
-* @i: Pointer to the current index in the format string
-*
-* Description: This function checks the format specifier and calls the
-* corresponding function to print the argument. If the specifier is not
-* recognized, it prints the specifier as it is.
-*
-* Return: Number of characters printed for the format specifier
-*/
+ * handle_format - Handles format specifiers and prints the output
+ * @format: Format string containing the directives
+ * @arg_list: List of arguments
+ * @i: Pointer to the current index in the format string
+ *
+ * Return: Number of characters printed for the format specifier
+ */
 int handle_format(const char *format, va_list arg_list, int *i)
 {
-	int j, len = 0;
+    int j, len = 0;
 
-	print_data p_func[] = {
-		{"c", pr_char},
-		{"s", pr_string},
-		{"d", pr_int},
-		{"i", pr_int},
-		{NULL, NULL}
-	};
+    print_data p_func[] = {
+        {"c", pr_char},
+        {"s", pr_string},
+        {"d", pr_int},
+        {"i", pr_int},
+        {NULL, NULL}
+    };
 
-	for (j = 0; p_func[j].type != NULL; j++)
-	{
-		if (format[*i + 1] == p_func[j].type[0])
-		{
-			len += p_func[j].print(arg_list);
-			(*i)++;
-			return (len);
-		}
-	}
+    for (j = 0; p_func[j].type != NULL; j++)
+    {
+        if (format[*i + 1] == p_func[j].type[0])
+        {
+            len += p_func[j].print(arg_list);
+            (*i)++;
+            return (len);
+        }
+    }
 
-	if (format[*i + 1] == '%')
-	{
-		len += print_char('%');
-		(*i)++;
-	}
-	else
-	{
-		len += print_char('%');
-		len += print_char(format[*i + 1]);
-		(*i)++;
-	}
+    if (format[*i + 1] == '%')
+    {
+        len += print_char('%');
+        (*i)++;
+    }
+    else
+    {
+        len += print_char('%');
+        len += print_char(format[*i + 1]);
+        (*i)++;
+    }
 
-	return (len);
+    return (len);
 }
 
 /**
-* print_char - Prints a single character
-* @c: Character to print
-*
-* Return: Always 1 (number of characters printed)
-*/
+ * print_char - Prints a single character
+ * @c: Character to print
+ *
+ * Return: Always 1 (number of characters printed)
+ */
 int print_char(char c)
 {
-	_putchar(c);
-	return (1);
+    _putchar(c);
+    return (1);
 }
