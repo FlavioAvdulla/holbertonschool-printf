b76696330930d2654363a2a0eaa148f8256aee2e
StilianSaka <stilisaka@gmail.com>
2024-07-26 07:04:38 +0000

    creating the header file

---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+
+/**
+ * struct print_data - structure for printing data
+ * @type: type of format
+ * @print: function for printing
+ */
+typedef struct print_data
+{
+	char *type;
+	int (*print)(va_list arg_list);
+} print_data;
+
+int _putchar(char c);
+
+int pr_char(va_list char_list);
+int pr_int(va_list int_list);
+int pr_string(va_list string_list);
+
+int _printf(const char *format, ...);
+
+#endif
+
